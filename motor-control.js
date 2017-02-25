adca443bcee46b4b6ac3c29f4bccc9c0f5d9b99b
Hsu <edittye379@gmail.com>
2017-02-25 10:38:51 +0800

    :sparkles:

--- motor-control.js
+++ motor-control.js
@@ -18,30 +18,62 @@ function stop() {
     rpio.write(rpin1, rpio.LOW);
     rpio.write(rpin2, rpio.LOW);
 }
-
+const sleepTime = 500
 function forward() {
     rpio.write(lpin1, rpio.HIGH);
     rpio.write(lpin2, rpio.LOW);
     rpio.write(rpin1, rpio.HIGH);
     rpio.write(rpin2, rpio.LOW);
-    rpio.msleep(1000);
+    rpio.msleep(sleepTime);
+    stop()
+}
+function backword() {
+    rpio.write(lpin1, rpio.LOW);
+    rpio.write(lpin2, rpio.HIGH);
+    rpio.write(rpin1, rpio.LOW);
+    rpio.write(rpin2, rpio.HIGH);
+    rpio.msleep(sleepTime);
+    stop()
+}
+function trunright() {
+    rpio.write(lpin1, rpio.HIGH);
+    rpio.write(lpin2, rpio.LOW);
+    rpio.write(rpin1, rpio.LOW);
+    rpio.write(rpin2, rpio.LOW);
+    rpio.msleep(sleepTime);
+    stop()
+}
+function trunleft() {
+    rpio.write(lpin1, rpio.LOW);
+    rpio.write(lpin2, rpio.LOW);
+    rpio.write(rpin1, rpio.HIGH);
+    rpio.write(rpin2, rpio.LOW);
+    rpio.msleep(sleepTime);
     stop()
 }
 
 process.stdin.on('keypress', function (ch, key) {
-  //console.log('got "keypress"', key);
-  switch(key.name) {
-    case 'w':
-        forward();
-        break;
-    default:
-        break;
-  }
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
-  }
+    //console.log('got "keypress"', key);
+    switch(key.name) {
+        case 'w':
+            forward();
+            break;
+        case 's':
+            backword();
+            break;
+        case 'a':
+            trunleft();
+            break;
+        case 'd':
+            trunright();
+            break;
+        default:
+            break;
+    }
+    if (key && key.ctrl && key.name == 'c') {
+        process.stdin.pause();
+    }
 });
- 
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
